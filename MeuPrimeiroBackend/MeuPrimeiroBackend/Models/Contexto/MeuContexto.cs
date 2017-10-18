5c7af967a90e6c7aa142db5ae8cfa1df417326ad
amanda thays <amanda-thaiss@hotmail.com>
2017-10-18 21:08:58 -0200

    Projeto inicial Foi adicionado Backend e contexto.

---
+++ MeuPrimeiroBackend/MeuPrimeiroBackend/Models/Contexto/MeuContexto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MeuPrimeiroBackend.Models.Contexto
+{
+    public class MeuContexto : DbContext
+    {
+        public MeuContexto() : base("strConn")
+        {
+
+        }
+    }
+}
